dc03dfb1c868964d622a4ed5d7ca9b8af74264da
Sudeep U <Sudeep7766@gmail.com>
2024-10-31 20:07:41 +0530

    secure-1

---
+++ src/main/java/com/example/securitytutorial/SecuritytutorialApplication.java
@@ -0,0 +1,13 @@
+package com.example.securitytutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuritytutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecuritytutorialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/securitytutorial/SecuritytutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.securitytutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuritytutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
