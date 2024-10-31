096dcb91a7a0e099dc6e8d823827a36fd2914189
Sudeep U <Sudeep7766@gmail.com>
2024-10-31 23:26:32 +0530

    filter_added

---
+++ src/main/java/com/example/securitytutorial/Filters/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.example.securitytutorial.Filters;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated());
+        //http.formLogin(withDefaults());
+        http.sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        http.httpBasic(withDefaults());
+        return http.build();
+    }
+}
